87b67de468dc265dae368b38a097d9ae41421683
Parham Fallah <parhamf7@gmail.com>
2024-06-15 14:33:18 +0330

    add main files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 
 import { EXPRESS_APP } from './core/secrets/index.js'
 
+import { router as usersRouter } from './modules/users/routes.js'
+import { router as productsRouter } from './modules/products/routes.js'
+
 const app = express();
 const serverPort = EXPRESS_APP.port;
 
@@ -13,6 +16,9 @@ app.use(express.json());
 app.get('/test', (req, res) => {
   res.json({message: 'Server is Running.'})
 });
+
+app.use(usersRouter);
+app.use(productsRouter);
 
 app.listen(serverPort, () => {
   console.log(`server is running on port ${serverPort}`);
